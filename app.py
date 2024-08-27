f69b54567c900fc24fea1f2dd59e98e40f301449
Diana Cardenas <dianacardenascastroc@gmail.com>
2024-08-26 22:42:39 -0500

    version final

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-
+st.title("Proyecto sprint 6")
 st.header("My new proyect _Diana_", divider="gray")
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma 1') # crear un botón
-hist_button2 = st.button('Construir histograma 2') # crear un botón
+hist_button = st.button('Construir histograma') # crear un botón
+hist_button2 = st.button('Construir grafico de dispersion') # crear un botón
 
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
@@ -19,9 +19,6 @@ if hist_button: # al hacer clic en el botón
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-st.header("", divider=True)
-
-
 if hist_button2:
     st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
     fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
@@ -32,3 +29,7 @@ build_histogram = st.checkbox('Construir un histograma')
 
 if build_histogram: # si la casilla de verificación está seleccionada
     st.write('Construir un histograma para la columna odómetro')
+
+
+
+st.caption(":sunglasses: :sunglasses: :sunglasses:")
